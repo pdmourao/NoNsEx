Fixed a bug where J would not update if computation was interrupted. pt2

MCfuncs.py
@@ -156,7 +156,7 @@ def MC2d_Lb(neurons, K, rho, M, lmb, dynamic, noise_dif, sigma_type, quality, n_
         with tqdm(total=len_l * len_y, disable=disable) as pbar:
 
             for idx_l, lmb_v in enumerate(lmb):
-                if len(mattis_flat) < idx_l*len_y:
+                if len(mattis_flat) < (idx_l+1)*len_y:
                     g = np.array([[1, - lmb_v, - lmb_v],
                               [- lmb_v, 1, - lmb_v],
                               [- lmb_v, - lmb_v, 1]])
@@ -169,11 +169,6 @@ def MC2d_Lb(neurons, K, rho, M, lmb, dynamic, noise_dif, sigma_type, quality, n_
                     try:
                         mattis[idx_s, idx_l, idx_y] = mattis_flat[idx_l * len_y + idx_y]
                     except IndexError:
-                        if J_lmb is None:
-                            g = np.array([[1, - lmb_v, - lmb_v],
-                                          [- lmb_v, 1, - lmb_v],
-                                          [- lmb_v, - lmb_v, 1]])
-                            J_lmb = gJprod(g, system.J)
                         new_inputs[y_arg] = y_v
                         output = system.simulate(J=J_lmb, dynamic=dynamic, cut=True,
                                                  sim_rngSS = rng_seeds[idx_l * len_y + idx_y], **new_inputs)[0]