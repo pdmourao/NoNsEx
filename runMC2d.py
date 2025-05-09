2c1bcd54c5b5d82fbba2ef8c13b5aca7892a2c89
pdmourao <pdmourao@gmail.com>
2025-05-09 18:34:34 +0200

    samples

--- runMC2d.py
+++ runMC2d.py
@@ -2,7 +2,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 from MCfuncs import MC2d, gridvec_toplot
 from time import time
-import FPfuncs as fp
 
 t0 = time()
 
@@ -49,7 +48,7 @@ m_array_trials, n_array_trials = MC2d(directory = 'MC2d', disable = disable, n_s
                                       y_arg = y_arg, x_values = x_values, y_values = y_values, **kwargs)
 
 cutoff = 0.8
-print(m_array_trials)
+# print(m_array_trials)
 
 fig, ax = plt.subplots(1)
 gridvec_toplot(ax, 'dis', m_array_trials, x_arg = x_arg, y_arg = y_arg, limx0 = x_values[0], limx1 = x_values[-1],
@@ -63,7 +62,7 @@ if y_arg == 'lmb':
 ax.set_xlabel(rf'$\{x_arg}$')
 ax.set_ylabel(rf'$\{y_arg}$')
 
-beta_title = '\infty' if np.isinf(kwargs['beta']) else kwargs['beta']
+beta_title = rf'\infty' if np.isinf(kwargs['beta']) else kwargs['beta']
 
 ax.set_title(rf'$\beta = {beta_title}$')
 plt.show()
